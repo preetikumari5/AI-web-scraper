0fd4834b007a722dadf6e5e6b72eda39e7752d1f
preetikumari5 <147963preetikumari@gmail.com>
2025-06-11 12:42:45 +0530

    updated project

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service
 import time
+from webbrowser import BeautifulSoup
 
 def scrape_website(website):
     print("Launching chrome browser...")
@@ -17,3 +18,28 @@ def scrape_website(website):
         return html
     finally:
         driver.quit()
+
+def extract_body_content(html_content):
+    soup = BeautifulSoup(html_content,"html.parser")
+    body_content = soup.body
+    if body_content:
+        return str(body_content)
+    return ""
+
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content,"html.parser")
+
+    for script_or_style in soup(["script","style"]):
+        script_or_style.extract()
+
+    cleaned_content = soup.get(separator="\n")
+    cleaned_content = "\n".join(
+        line.strip() for line in cleaned_content.splitlines() if line.strip()
+    )
+
+    return cleaned_content
+
+def split_dom_content(dom_content,max_length = 6000):
+    return [
+        dom_content[i: i+max_length] for i in range(0,len(dom_content),max_length)
+    ]
