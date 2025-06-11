87754272b99991afee261a733997a01c24170384
preetikumari5 <147963preetikumari@gmail.com>
2025-06-11 12:41:20 +0530

    updated project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from scrape import scrape_website
+from webbrowser import BeautifulSoup
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter a Website URL:")
@@ -8,3 +9,29 @@ if st.button("Scrape Site"):
     st.write("Scraping the website")
     result = scrape_website(url)
     print(result)
+
+def extract_body_content(html_content):
+    soup = BeautifulSoup(html_content,"html.parser")
+    body_content = soup.body
+    if body_content:
+        return str(body_content)
+    return ""
+
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content,"html.parser")
+
+    for script_or_style in soup(["script","style"]):
+        script_or_style.extract()
+
+    cleaned_content = soup.get(separator="\n")
+    cleaned_content = "\n".join(
+        line.strip() for line in cleaned_content.splitlines() if line.strip()
+    )
+
+    return cleaned_content
+
+def split_dom_content(dom_content,max_length = 6000):
+    return [
+        dom_content[i: i+max_length] for i in range(0,len(dom_content),max_length)
+    ]
+
